8c679cac5e31f8da79f619baf76b7efe6899c011
Leo <icefoxz@msn.com>
2021-08-28 10:21:40 +0800

    修复密码显示问题

--- SanGuoHuiJuanPj/Assets/Scripts/UIControl/LoginUiController.cs
+++ SanGuoHuiJuanPj/Assets/Scripts/UIControl/LoginUiController.cs
@@ -153,8 +153,8 @@ public class LoginUiController : MonoBehaviour
     private void InitAccountInfo()
     {
         //accountInfo.warningMessage.gameObject.SetActive(!GamePref.IsUserAccountCompleted);
-        if (GamePref.IsUserAccountCompleted) accountInfo.password.text = GamePref.Password;
-        accountInfo.password.contentType = isDeviceLogin ? InputField.ContentType.Password : InputField.ContentType.Standard;
+        var isDefaultPwd = accountInfo.password.text == DeviceLoginString;
+        accountInfo.password.contentType = isDefaultPwd ? InputField.ContentType.Password : InputField.ContentType.Standard;
         accountInfo.backBtn.onClick.AddListener(Close);
         accountInfo.changePasswordBtn.onClick.AddListener(() => OnAction(ActionWindows.ChangePassword));
     }
@@ -275,7 +275,11 @@ public class LoginUiController : MonoBehaviour
         busyPanel.gameObject.SetActive(false);
         if (success)
         {
-            if (!isDeviceLogin) GamePref.SetPassword(password);
+            if (!isDeviceLogin)
+            {
+                GamePref.SetPassword(password);
+                accountInfo.password.text = password;
+            }
             UnityMainThread.thread.RunNextFrame(() =>
                 OnLoggedInAction.Invoke(info.Username, password, info.Arrangement, info.IsNewRegistered));
             return;
