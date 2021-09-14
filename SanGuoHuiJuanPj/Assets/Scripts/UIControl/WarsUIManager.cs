8252243386b424237ac8a12e3bb82e7ada602ad9
Leo <icefoxz@msn.com>
2021-09-14 11:42:50 +0800

    修复初始势力选择而导致战斗旗帜找不到表id的问题。

--- SanGuoHuiJuanPj/Assets/Scripts/UIControl/WarsUIManager.cs
+++ SanGuoHuiJuanPj/Assets/Scripts/UIControl/WarsUIManager.cs
@@ -1027,7 +1027,9 @@ public class WarsUIManager : MonoBehaviour
         string flagShort;
         if (py.Character != null && py.Character.IsValidCharacter() && !string.IsNullOrWhiteSpace(py.Character.Name))
             flagShort = py.Character.Name.First().ToString();
-        else flagShort = DataTable.PlayerInitialConfig[py.pyData.ForceId].Force;
+        else if(DataTable.PlayerInitialConfig.ContainsKey(py.pyData.ForceId)) 
+            flagShort =  DataTable.PlayerInitialConfig[py.pyData.ForceId].Force;
+        else flagShort = "玩";
         infoUis.Short.text = flagShort;
         UpdateInfoUis();
         UpdateLevelInfo();
