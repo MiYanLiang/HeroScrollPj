d33033af086dd44afcf53b0c751c7da6ecf48122
Leo <icefoxz@msn.com>
2021-11-10 18:10:16 +0800

    修复业火不会全屏问题

--- SanGuoHuiJuanPj/Assets/Scripts/System/WarModule/ChessSprite.cs
+++ SanGuoHuiJuanPj/Assets/Scripts/System/WarModule/ChessSprite.cs
@@ -531,7 +531,7 @@ namespace Assets.System.WarModule
             new int[] { 7 },
             new int[] { 2, 5, 6, 10, 11, 12 },
             new int[] { 0, 1, 3, 4, 8, 9, 13, 14, 15, 16, 17 },
-            new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 },
+            new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 },
         };
 
         public override int TypeId { get; } = (int)Kinds.YeHuo;
